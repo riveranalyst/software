356807632b3286196bb03bee69e60b4285449653
Beatriz Negreiros (IWS) <beatriznegreiros@outlook.com>
2022-09-07 18:41:23 +0200

    fixed merging issue due to float type sed depth

--- flussdata/utils/tables_merge.py
+++ flussdata/utils/tables_merge.py
@@ -18,14 +18,11 @@ kf_objects = models.Kf.objects.all()
 
 ido_df = read_frame(ido_objects)
 st_df = read_frame(st_objects)
-# st_df.rename(columns={'name': 'meas_station'}, inplace=True)
-
-new_df = st_df.merge(ido_df, left_on='name', right_on='meas_station')
-new_df.to_csv('trial-merge-dt.csv')
-
 kf_df = read_frame(kf_objects)
 
 # option how=outer is to ensure the union instead of intersection of the two dfs
-kf_idoc = ido_df.merge(kf_df, on=['sample_id', 'sediment_depth_m'], how='outer')
-kf_idoc = kf_idoc.drop_duplicates(subset=['sample_id', 'sediment_depth_m'])
-kf_idoc.to_csv('trial-idoc-kf.csv')
+kf_idoc = ido_df.merge(kf_df, on=['sample_id', 'dp_position'], how='outer')
+kf_idoc = kf_idoc.drop_duplicates(subset=['sample_id', 'dp_position'])
+
+kf_idoc_st = st_df.merge(ido_df, left_on='name', right_on='meas_station')
+kf_idoc_st.to_csv('merge-kf-idoc-st.csv')
