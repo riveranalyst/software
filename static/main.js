565b2b5becd0b85f6203009ad277afa0aa40ee7e
Beatriz Negreiros (IWS) <beatriznegreiros@outlook.com>
2022-09-08 12:47:03 +0200

    updt requir.txt

--- static/main.js
+++ static/main.js
@@ -15,12 +15,30 @@ $(function() {
         window.location.href="/flussdata/modify/upload/success_upload/"},
          2000)});
 })
-
-$('code').on('click', function(e){
-  var sel = window.getSelection()
-  var r = document.createRange()
-  r.selectNodeContents(e.target.closest('code'))
-  sel.removeAllRanges()
-  sel.addRange(r)
-  document.execCommand('copy')
-})
+//var options = {
+//    // the selector for the badge template
+//    templateSelector: "#CodeBadgeTemplate",
+//
+//    // base content CSS selector that is searched for snippets
+//    contentSelector: "body",
+//
+//    // Delay in ms used for `setTimeout` before badging is applied
+//    // Use if you need to time highlighting and badge application
+//    // since the badges need to be applied afterwards.
+//    // 0 - direct execution (ie. you handle timing
+//    loadDelay: 0,
+//
+//    // CSS class(es) used to render the copy icon.
+//    copyIconClass: "fa fa-copy",
+//    // optional content for icons class (<i class="fa fa-copy"></i> or <i class="material-icons">file_copy</i>)
+//    copyIconContent: "",
+//
+//    // CSS class(es) used to render the done icon.
+//    checkIconClass: "fa fa-check text-success",
+//    checkIconContent: "",
+//
+//    // function called before code is placed on clipboard that allows you inspect and modify
+//    // the text that goes onto the clipboard. Passes text and code root element (hljs).
+//    // Example:  function(text, codeElement) { return text + " $$$"; }
+//    onBeforeCodeCopied: null
+//};
