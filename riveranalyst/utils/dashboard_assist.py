952b662ce2e64747ffe88270174bed4adeca2847
Beatriz Negreiros (IWS) <beatriznegreiros@outlook.com>
2023-03-17 14:36:02 +0100

    mplt plot funcs for global analyses

--- riveranalyst/utils/dashboard_assist.py
+++ riveranalyst/utils/dashboard_assist.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 from django_pandas.io import read_frame
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import MinMaxScaler
 from riveranalyst.models import *
 
 
@@ -33,7 +35,8 @@ def get_corr_fig():
 
     # correlation
     takeoff_cols = ['id_surf', 'id_subsurf', 'id_x', 'id_y', 'id', 'x', 'y', 'x_epsg', 'y_epsg', 'x_epsg4326', 'y_epsg4326']
-    df_corr = df_global.loc[:, ~df_global.columns.isin(takeoff_cols)].corr().round(1)
+    df_final = df_global.loc[:, ~df_global.columns.isin(takeoff_cols)]
+    df_corr = df_final.corr().round(1)
 
     # mask to matrix
     mask = np.zeros_like(df_corr, dtype=bool)
@@ -41,5 +44,72 @@ def get_corr_fig():
 
     # viz
     df_corr_viz = df_corr.mask(mask).dropna(how='all').dropna('columns', how='all')
-    fig = px.imshow(df_corr_viz, text_auto=True)
-    return fig
+    fig = px.imshow(df_corr_viz, text_auto=True, width=1000, height=1000)
+    return fig, df_final
+
+
+def get_PCA(df):
+    # Preparing df for dimensinality reduction
+    features = ['idoc_mgl', 'wl_m', 'kf_ms', 'temp_c', 'H_m_x', 'river',
+                'n_wooster', 'd10', 'd50', 'd90', 'so', 'dm', 'dg',
+                'percent_finer_2mm', 'percent_finer_1mm', 'percent_finer_0_5mm']
+    df4pca = df[features].dropna()
+    df4pca_prescaling = df4pca.drop(['river'], axis=1)
+
+    # Scaling with s-score apporach
+    df4pca_final = df4pca_prescaling.apply(lambda x: (x - x.mean()) / x.std(), axis=0)
+
+    # Build PCA
+    pca = PCA()
+    components = pca.fit_transform(df4pca_final)
+
+    # Get labels for explaind variance respective tot he PC
+    labels = {
+        str(i): f"PC {i + 1} ({var:.1f}%)"
+        for i, var in enumerate(pca.explained_variance_ratio_ * 100)
+    }
+
+    # Plot data 2-dimensionaly in the new coordinate system of PCs
+    fig2d = px.scatter_matrix(
+        components,
+        labels=labels,
+        dimensions=range(3),
+        color=df4pca["river"]
+    )
+    fig2d.update_traces(diagonal_visible=False)
+
+    # Plot data 3-dimensionaly in the new coordinate system of PCs
+    pca3d = PCA(n_components=3)
+    components3d = pca3d.fit_transform(df4pca_final)
+    total_var = pca3d.explained_variance_ratio_.sum() * 100
+    fig3d = px.scatter_3d(
+        components3d, x=0, y=1, z=2, color=df4pca['river'],
+        title=f'Total Explained Variance: {total_var:.2f}%',
+        labels={'0': 'PC 1', '1': 'PC 2', '2': 'PC 3'}
+    )
+
+    # Visualizing loadings of each components
+    loadings = pca3d.components_.T * np.sqrt(pca3d.explained_variance_)
+    fig_load = px.scatter(components3d, x=0, y=1, color=df4pca['river'], width=1000, height=600)
+    for i, feature in enumerate(df4pca_final.columns):
+        fig_load.add_annotation(
+            ax=0, ay=0,
+            axref="x", ayref="y",
+            x=loadings[i, 0]*4,
+            y=loadings[i, 1]*4,
+            showarrow=True,
+            arrowsize=2,
+            arrowhead=2,
+            xanchor="right",
+            yanchor="top"
+        )
+        fig_load.add_annotation(
+            x=loadings[i, 0]*4,
+            y=loadings[i, 1]*4,
+            ax=0, ay=0,
+            xanchor="center",
+            yanchor="bottom",
+            text=feature,
+            yshift=5,
+        )
+    return fig2d, fig3d, fig_load
